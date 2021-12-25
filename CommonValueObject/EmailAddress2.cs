de3d74ccceb1ce1ba3a6bfc8a28be6f56e37b284
Xavier <xavierjohn@hotmail.com>
2021-12-25 08:30:39 -0800

    Using ValidationResult.

---
+++ CommonValueObject/EmailAddress2.cs
@@ -0,0 +1,34 @@
+﻿namespace CommonValueObject
+{
+    using CSharpFunctionalExtensions;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class EmailAddress2 : ValueObject
+    {
+        [Required, EmailAddress]
+        public string Email { get; }
+
+        private EmailAddress2(string value)
+        {
+            Email = value;
+        }
+
+        public static Result<EmailAddress2, List<ValidationResult>> Create(string strEmail)
+        {
+            var model = new EmailAddress2(strEmail);
+            var context = new ValidationContext(model, serviceProvider: null, items: null);
+            var errorResults = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(model, context, errorResults, true))
+            {
+                return Result.Failure<EmailAddress2, List<ValidationResult>>(errorResults);
+            }
+            return model;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Email;
+        }
+    }
+}
--- CommonValueObjectTests/EmailAddress2Test.cs
+++ CommonValueObjectTests/EmailAddress2Test.cs
@@ -4,23 +4,25 @@ namespace CommonValueObjectTests
     using FluentAssertions;
     using Xunit;
 
-    public class EmailAddresssTest
+    public class EmailAddress2Test
     {
         [Theory]
         [InlineData("a")]
         [InlineData("@y.com")]
         public void Invaild_email_address_should_fail(string strEmail)
         {
-            var result = EmailAddress.Create(strEmail);
+            var result = EmailAddress2.Create(strEmail);
 
             result.IsFailure.Should().BeTrue();
+            result.Error.Count.Should().Be(1);
+            result.Error[0].ErrorMessage.Should().Be("The Email field is not a valid e-mail address.");
         }
 
         [Theory]
         [InlineData("x@y.com")]
         public void Vaild_email_address_should_pass(string strEmail)
         {
-            var result = EmailAddress.Create(strEmail);
+            var result = EmailAddress2.Create(strEmail);
 
             result.IsSuccess.Should().BeTrue();
         }
